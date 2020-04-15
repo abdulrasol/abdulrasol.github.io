b44ab29a116c3b9a8fe17f784ee8528c5040d533
AbdulRasol S. Sajjad <abdulrsol97@gmail.com>
2020-04-15 18:06:09 +0300

    add mobile

--- spimebook/js/plugin.js
+++ spimebook/js/plugin.js
@@ -1,6 +1,23 @@
 /*global $, window */
 $('document').ready(function () {
     'use strict';
-   
-   
+    var DOCUMENT = $('window');
+    var mobile_navbar = $('#me-mobile-navbar');
+    var main_navbar = $('#me-main-navbar')
+    $(window).on('scroll', function(event) {
+			var sc = $(window).scrollTop();
+      if (sc >= 80) {
+        mobile_navbar.css({
+          position: 'fixed',
+          top:'2px',
+          left:'0px'
+        });
+      } else {
+        mobile_navbar.css({
+          position: 'relative',
+          top:'0px'
+        });
+      }
+
+			});
 });
