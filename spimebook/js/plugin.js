a30c11f457512e53f67ddab30e459469752dea71
abdulrasol <abdulrsol97@gmail.com>
2020-04-19 00:49:22 +0300

    add

--- spimebook/js/plugin.js
+++ spimebook/js/plugin.js
@@ -1,23 +1,24 @@
 /*global $, window */
 $('document').ready(function () {
-    'use strict';
-    var DOCUMENT = $('window');
-    var mobile_navbar = $('#me-mobile-navbar');
-    var main_navbar = $('#me-main-navbar')
-    $(window).on('scroll', function(event) {
-			var sc = $(window).scrollTop();
-      if (sc >= 80) {
-        mobile_navbar.css({
-          position: 'fixed',
-          top:'2px',
-          left:'0px'
-        });
-      } else {
-        mobile_navbar.css({
-          position: 'relative',
-          top:'0px'
-        });
-      }
+  'use strict';
+  // Mobile Nav-bar
+  var DOCUMENT = $('window');
+  var mobile_navbar = $('#me-mobile-navbar');
+  var main_navbar = $('#me-main-navbar')
+  $(window).on('scroll', function (event) {
+    var sc = $(window).scrollTop();
+    if (sc >= 80) {
+      mobile_navbar.css({
+        position: 'fixed',
+        top: '2px',
+        left: '0px'
+      });
+    } else {
+      mobile_navbar.css({
+        position: 'relative',
+        top: '0px'
+      });
+    }
 
-			});
+  });
 });
