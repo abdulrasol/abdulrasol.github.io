7378015dfa31bd280244942400c5218fbed94be1
abdulrasol <abdulrsol97@gmail.com>
2020-05-22 20:42:43 +0300

    G

--- m1/js/plugin.js
+++ m1/js/plugin.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-  $('.news').slick({
-    infinite: true,
-    speed: 300,
-    autoplay:true,
-});
-  $('.slider').slick({
-    infinite: true,
-    speed: 300,
-    autoplay:true,
-});
+	$('.news').slick({
+		infinite: true,
+		speed: 10000,
+		autoplay:true,
+		accessibility:true,
+		autoplaySpeed: 0,
+		cssEase: 'linear',
+		pauseOnHover:true,
+		mobileFirst:true
+	});
+	$('.slider').slick({
+		infinite: true,
+		speed: 300,
+		autoplay:true,
+	});
 
 });
