c2e1d8b81bf97b3e47106731a6f1e092126e08fc
abdulrasol <abdulrsol97@gmail.com>
2020-05-22 21:59:01 +0300

    G

--- m1/js/plugin.js
+++ m1/js/plugin.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
 	$('.news').slick({
 		infinite: true,
-		speed: 10000,
+		speed: 2000,
 		autoplay:true,
 		accessibility:true,
-		autoplaySpeed: 0,
+		autoplaySpeed: 3000,
 		cssEase: 'linear',
 		pauseOnHover:true,
 		mobileFirst:true
 	});
 	$('.slider').slick({
 		infinite: true,
-		speed: 300,
+		speed: 1500,
 		autoplay:true,
+		autoplaySpeed: 3000,
 	});
 
 });
